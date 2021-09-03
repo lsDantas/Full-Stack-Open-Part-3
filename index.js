015a09b3ab616fa41928bd92c5ad89087bc205f4
Leo <leonardosdantas@protonmail.com>
2021-09-03 17:58:48 -0300

    Exercise 3.8

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 var morgan = require('morgan');
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('data', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 let persons = [
     {
