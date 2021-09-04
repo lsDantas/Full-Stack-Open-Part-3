62a5a89cdc9a9940b443cc19b96ed6d248e328c5
Leo <leonardosdantas@protonmail.com>
2021-09-04 20:29:13 -0300

    Exercise 3.20

--- models/person.js
+++ models/person.js
@@ -18,11 +18,13 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minLength: 3,
         unique: true
     },
     number: {
         type: String,
-        required: true
+        required: true,
+        minLength: 8
     } 
 });
 
