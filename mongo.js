568be9ae6b34f50ce6e6cc1e853d6ebb04ebba29
Leo <leonardosdantas@protonmail.com>
2021-09-03 23:47:08 -0300

    Fixed Exercise 3.12

--- mongo.js
+++ mongo.js
@@ -4,29 +4,47 @@ const mongoose = require('mongoose');
 if (process.argv.length < 3) {
     console.log("Please provide the password as an argument: node mongo.js <password>");
 }
+else {
+    // Setup Database
+    const dbPassword = process.argv[2];
+    const url = `mongodb+srv://fullstack:${dbPassword}@cluster0.6uwcj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+    mongoose.connect(url);
 
-// Setup Database
-const dbPassword = process.argv[2];
-const url = `mongodb+srv://fullstack:${dbPassword}@cluster0.6uwcj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+    // Establish Person Schema
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: String
+    });
 
-mongoose.connect(url);
+    const Person = mongoose.model("Person", personSchema);
 
-// Establish Person Schema
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-});
+    
+    if (process.argv.length == 3) {
+        // Display Contacts in Database
+        Person.find({}).then(result => {
+            console.log("phonebook");
+            result.forEach(person => {
+                console.log(`${person.name} ${person.number}`);
+            })
+            mongoose.connection.close()
+        })
+    }
+    else {
+        // Add New Contact
+        if (process.argv.length == 5 ) {
+            
+            const person = new Person({
+                name: process.argv[3],
+                number: process.argv[4]
+            });
 
-const Person = mongoose.model("Person", personSchema);
+            // Save New Entry
+            person.save().then(result => {
+                console.log(`Added ${person.name} number ${person.number} to phonebook`);
+                mongoose.connection.close()
+            });
+        }
+    }
+}
 
-const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
-});
 
-// Save New Entry
-person.save().then(result => {
-    console.log('Person saved!');
-    console.log(JSON.stringify(person));
-    mongoose.connection.close()
-});
